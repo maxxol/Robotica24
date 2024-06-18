471d3c0bf8d9a31af7e921ffba501f4da95260b9
JordiV27 <jordi2global@gmail.com>
2024-06-18 09:39:21 +0200

    shutdown and voltage scripts

--- roboticaSourceCode/python/voltageCheck.py
+++ roboticaSourceCode/python/voltageCheck.py
@@ -1,19 +1,40 @@
 import sys
 from pyax12.connection import Connection
 import RPi.GPIO as gpio
+import subprocess
 
 dyx_idBase = 2 #arbitrary servo, just need one and this one is the first in 
-
+shutdown_script = "../shell/shutdown.sh"
 
 #gripper rotate
 sc = Connection(port="/dev/serial0",baudrate=1000000, rpi_gpio=True)
 
-def getVoltage():
+#gpio shutdown
+shutdown_pin = 30 #does not exist, just a placeholder
+
+voltage_tolerance = 2
+
+def getVoltage(dyx_idBase):
     #check voltage
+    current_voltage = sc.get_present_voltage(dyx_idBase)
+
+    max_voltage = sc.get_max_voltage(dyx_idBase)
+    min_voltage = sc.get_min_voltage(dyx_idBase)
 
 	#if bad: sudoshutdown shell file
+    if (current_voltage >  max_voltage - voltage_tolerance) or (current_voltage < min_voltage + voltage_tolerance):
+        shutdown()
+
 	#if knop: sudoshutdown shell file
-    return
+    state = gpio.input(shutdown_pin)
+    if state:
+        shutdown()
 
 
-getPosition()
+def shutdown():
+    sub_proc = subprocess.Popen(shutdown_script, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    
+
+
+getVoltage()
---
+++ roboticaSourceCode/shell/shutdown.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo shutdown -hP now
+
+
+# in linux you need to make this script an executable: chmod +x shutdown.sh
